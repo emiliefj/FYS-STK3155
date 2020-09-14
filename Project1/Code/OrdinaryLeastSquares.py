09f174e831628037efbd2c0d4e9023884ea8f681
Emilie Fjørner <emilie.fjorner@gmail.com>
2020-09-14 15:25:56 +0200

    Added method for bootstrap calculating a statisic given as input.

--- Project1/Code/OrdinaryLeastSquares.py
+++ Project1/Code/OrdinaryLeastSquares.py
@@ -1,4 +1,5 @@
-import numpy as np 
+import numpy as np
+from numpy.random import randint
 
 class OrdinaryLeastSquares:
     ''' Perform OLS regression and assess performance.
@@ -13,6 +14,7 @@ class OrdinaryLeastSquares:
     def __init__(self,X,z):
         self.X = X
         self.z = z
+        self.n = np.size(z)
         # self.beta = np.linalg.inv(self.X.T.dot(self.X)).dot(self.X.T).dot(self.z)
         # self.ztilde = self.X.dot(beta)
 
@@ -30,9 +32,24 @@ class OrdinaryLeastSquares:
         self.ztilde = self.X.dot(beta)
         return ztilde
 
+    def bootstrap(self,B,statistic):
+        '''Resampling using the bootstrap method
+
+        Calculates an estimate for the statistic given as input,
+        on the data z.
+
+        B         - Number of bootstraps
+        statistic - the statistic to be estimated
+        returns an array of the calculated theta values
+        '''
+        theta = zeros(B)
+        for i in range(B):
+            theta[i] = statistic(z[randint(0,self.n,self.n)]) # Randomly select n vars from z
+
+
     def mean_square_error(self):
         ''' Returns the Mean Square Error of z.'''
-        return np.sum((self.z-self.ztilde)**2)/np.size(self.z)
+        return np.sum((self.z-self.ztilde)**2)/self.n
 
     def R2(self):
         '''Returns the R**2 value for z.'''
@@ -40,6 +57,15 @@ class OrdinaryLeastSquares:
 
     def mean(self):
         ''' Returns the mean of the values of the array z.'''
-        return np.sum(self.z)/np.size(self.z)
+        return np.sum(self.z)/self.n
+
+    def variance_of_beta(self):
+        self.var_of_beta = self.sigma2*np.linalg.inv(self.X.T.dot(self.X))
+
+    def sigma2(self):
+        '''Finds the variance of z.'''
+        # How?
+
+
 
 
