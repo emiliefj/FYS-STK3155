e3a83d50a5995ccb2d4cf09b1be0eb6b8badf23a
Emilie Fjørner <emilie.fjorner@gmail.com>
2020-09-14 14:27:16 +0200

    Added a class for performing OLS regression. Included some common error measures.

---
+++ Project1/Code/OrdinaryLeastSquares.py
@@ -0,0 +1,41 @@
+import numpy as np 
+
+class OrdinaryLeastSquares:
+    ''' Perform OLS regression and assess performance.
+
+    My own code for performing regression using Ordinary Least 
+    Squares (OLS)
+    Including resampling techniques:
+    - K-fold Cross Validation (KFoldCV)
+    - Bootstrap
+    '''
+
+    def __init__(self,X,z):
+        self.X = X
+        self.z = z
+
+    def regress(self):
+        ''' Regression using Ordinary Least Squares.
+
+        X     - The design matrix
+        z     - The output vars
+
+        @return the prediction for the output, ztilde
+        '''
+        self.beta = np.linalg.inv(self.X.T.dot(self.X)).dot(self.X.T).dot(self.z)
+        self.ztilde = self.X.dot(beta)
+        return ztilde
+
+    def mean_square_error(self):
+        ''' Returns the Mean Square Error of z.'''
+        return np.sum((self.z-self.ztilde)**2)/np.size(self.z)
+
+    def R2(self):
+        '''Returns the R**2 value for z.'''
+        return 1-np.sum((self.z-self.ztilde)**2)/np.sum((self.z-self.mean(self.z))**2)
+
+    def mean(self):
+        ''' Returns the mean of the values of the array z.'''
+        return np.sum(self.z)/np.size(self.z)
+
+
