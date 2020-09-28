f6da1dd42723accb0d15e6b184d3d608a7416974
Emilie Fjørner <emilie.fjorner@gmail.com>
2020-09-28 15:42:37 +0200

    Added the project description

--- Project1/Code/FrankeFunction.py
+++ Project1/Code/FrankeFunction.py
@@ -3,12 +3,11 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import numpy as np
-#from random import random, seed
-#from numba import jit
+
 
 class FrankeFunction:
     '''
-    The franke function is a two dimensional weighted sum of four 
+    The Franke function is a two dimensional weighted sum of four 
     exponentials. It has two Gaussian peaks of different heights, 
     and a smaller dip and is often used as a test function in 
     interpolation problems.
@@ -20,22 +19,25 @@ class FrankeFunction:
     for interpolation of scattered data
     '''
     def __init__(self,x,y):
+        self.x = x
+        self.y = y
+        self.z = self.calculate_values(x,y)
+
+    def calculate_values(self,x,y):
         term1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2))
         term2 = 0.75*np.exp(-((9*x+1)**2)/49.0 - 0.1*(9*y+1))
         term3 = 0.5*np.exp(-(9*x-7)**2/4.0 - 0.25*((9*y-3)**2))
         term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
-        self.x = x
-        self.y = y
-        self.z = term1 + term2 + term3 + term4
+        return term1 + term2 + term3 + term4
     
     def get_values(self):
         return self.z
 
     def add_normal_noise(self,mean,variance):
         '''
-        Adds some noise to the data in the form of
-        random numbers following a normal distribution
-        with mean=0 and variance as given in the input.
+        Adds noise to the data in the form of random
+        numbers following a normal distribution with
+        mean and variance as given in the input.
 
         mean        -- the mean of the distribution
         variance     -- sigma**2 of the distribution
@@ -43,10 +45,17 @@ class FrankeFunction:
         self.z = self.z + np.random.normal(mean,variance,self.z.shape)
 
     def plot_function(self):
+        '''
+        Creates a meshgrid of the x and y values, 
+        calculates the corresponding z-values,
+        and plots the result as a surface plot.
+        '''
         fig = plt.figure()
         ax = fig.gca(projection='3d')
         # Plot the surface.
-        surf = ax.plot_surface(self.x, self.y, self.z, cmap=cm.coolwarm,
+        x_grid, y_grid = np.meshgrid(self.x,self.y)
+        z_grid = self.calculate_values(x_grid,y_grid)
+        surf = ax.plot_surface(x_grid, y_grid, z_grid, cmap=cm.coolwarm,
                        linewidth=0, antialiased=False)
         # Customize the z axis.
         ax.set_zlim(-0.10, 1.40)
@@ -58,40 +67,3 @@ class FrankeFunction:
 
         plt.show()
 
-# Make data
-x = np.arange(0, 1, 0.05)
-y = np.arange(0, 1, 0.05)
-x, y = np.meshgrid(x,y)
-franke_function = FrankeFunction(x, y)
-franke_function.add_normal_noise(0,0.01)
-franke_function.plot_function()
-
-
-# ----------
-
-# @jit
-def create_design_matrix(x,y,d): # Not sure where to put this yet
-    ''' Set up design matrix X
-
-    Builds the design matrix X for a polynomial of degree n
-    with two input variables x and y.
-
-    x     - first input variable (array of length N)
-    y     - second input variable (array of length N)
-    d     - the polynomial degree 
-    return the design matrix X
-    '''
-    n = len(x)
-    if(n!=len(y)):
-        raise Exception("Input args x and y differ in length")
-
-    p = int((d+1)*(d+2)/2)  # number of terms in the resulting polynomial                                                              
-    X = np.ones((n,p))      # X has dimensionality nxp
-
-    # Building X:
-    for i in range(1,d+1):
-        q = int((i)*(i+1)/2)
-        for j in range(i+1):
-            X[:,q+j] = (x**(i-j))*(y**(j))
-
-    return X
