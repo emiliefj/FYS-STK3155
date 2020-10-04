2cfe02ec0d120124dd111275858c2700972a4bf9
Emilie Fjørner <emilie.fjorner@gmail.com>
2020-10-04 13:04:50 +0200

    Added kFold CV to ridge code

--- Project1/Code/RidgeRegression.py
+++ Project1/Code/RidgeRegression.py
@@ -9,7 +9,7 @@ import Code.OrdinaryLeastSquares as OLS
 
 class RidgeRegression(OLS.OrdinaryLeastSquares):
 
-    def fit(self,X,z,alpha=1.0):
+    def fit(self,X,z,alpha=0.1):
         ''' Regression using Ordinary Least Squares.
 
         X       - The design matrix
@@ -28,3 +28,32 @@ class RidgeRegression(OLS.OrdinaryLeastSquares):
         self.is_regressed = True
        
         return self.ztilde
+
+    def k_fold_cv(self,input_data,target,k=5, alpha=0.1, shuffle=True):
+        ''' resampling using k-fold cross validation
+
+        input_data  - input data
+        target      - target data
+        k           - The number of folds
+        shuffle     - if True the dataset is shuffled before 
+                      splitting into folds
+        returns the average of the mean square errors found when predicting the test fold
+        '''
+
+        indices = np.array(range(0,input_data.shape[0]))
+        if(shuffle): # shuffle data before split
+            indices = random.sample(range(0,input_data.shape[0]), input_data.shape[0])
+
+        folds = np.array_split(indices, k)
+
+        mse = np.zeros(k)
+        for i in range(k):
+            train = folds.copy()
+            test = train[i]
+            del train[i]
+            train = np.concatenate(train)
+            self.fit(input_data[train],target[train],alpha)
+            target_hat = self.predict(input_data[test])
+            mse[i] = self.mean_square_error(target_hat,target[test])
+
+        return np.mean(mse)
